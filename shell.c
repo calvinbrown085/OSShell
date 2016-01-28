5fe52e42e0a0e095075fc855dfa9021fdfc6beb4
Calvin Brown <calvin@bannos-MBP.hitronhub.home>
2016-01-27 19:40:42 -0600

    Added the starting file of the shell.c

---
+++ shell.c
@@ -0,0 +1,90 @@
+/***********************************************************
+ * Name of program: Shell
+ * Authors: Chad Gilmer, Calvin Brown
+ * Description: This is a program that imitates parts of the linux shell
+ **********************************************************/
+
+/* These are the included libraries.  You may need to add more. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+/* I don't want to see any global variables in this program. -5 pts
+ * for any global variable used. */
+#define ARGV_SIZE 10
+
+/* Declarations go here for functions that are called before they
+ * are defined.  For example, the function named helper is called
+ * in main before it is defined, so I declare it here. 
+ */
+char tokenizeUserInput(char userInput[80]);
+
+
+
+/* This is the main function of your project, and it will be run
+ * first before all other functions.
+ */
+int main(int argc, char *argv[])
+{
+	char userInput[80] = "";
+
+	//char workingDirectory[1024] = getCurrentWorkingDirectory();
+        char *myArgv[ARGV_SIZE];  // an array of pointers to strings
+
+  	myArgv[0] = "ls";
+  	myArgv[1] = "-aux";
+  	myArgv[2] = NULL;  // last element should be a NULL pointer
+
+  	execvp(myArgv[0], myArgv);
+        //We just have user input and make sure that the user wants to keep entering words until they enter quit
+        while(1){
+        	printf("prompt$ ");
+                fgets(userInput, 80, stdin);
+
+
+                if(strcmp(userInput, "quit") == 10){
+                        printf("Exiting!\n");
+                        break;
+		}
+	//	tokenizeUserInput(userInput);
+		execvp(myArgv[0],myArgv);
+	}	
+	return 0; /* Success */
+}
+
+
+/* Comments describing what each function does must be included on 
+ * top of each function.  -5 pts for each undocumented function.
+ */
+char tokenizeUserInput(char userInput[80])
+{
+        char *list;
+        int count;
+        count = 0;
+        list = strtok(userInput," ");
+	
+        //This list just increments each time the strtok finds a word and will go until there is none left
+        while(list != NULL )
+        {
+		
+		printf("%s\n",list);
+                list = strtok(NULL, " ");
+		
+                count += 1;
+		
+        }
+        return 0;
+}
+
+int executeCommand(char command[15])
+{
+
+
+
+
+
+}
